5af3604f27af0ccd17fecc6ba4fa7b83c5363a01
Lee Pham <leepham@utexas.edu>
2020-12-05 23:52:42 -0600

    Refactored to use assignment express instead. Not sure how I feel about it...

--- 2020/1/day1.py
+++ 2020/1/day1.py
@@ -4,14 +4,16 @@ with open('day1.txt') as f:
     puzzle_input = f.read()
 
 expense_report = [int(x) for x in puzzle_input.split("\n")]
+
+
 for expense in expense_report:
     difference = 2020 - expense
     if difference in expense_report:
         print(expense * difference)
         break
 
-while True:
-    random3 = random.sample(expense_report, 3)
-    if sum(random3) == 2020:
-        print(random3[0] * random3[1] * random3[2])
-        break
+
+while sum(random3 := random.sample(expense_report, 3)) != 2020:
+    pass
+
+print(random3[0] * random3[1] * random3[2])
